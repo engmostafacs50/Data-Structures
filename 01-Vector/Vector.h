f21f2b6cd31d03e28c9a23e02d41c55b7c933d1d
most3f3.1112006@gmail.com <most3f3.1112006@gmail.com>
2026-02-01 00:15:22 +0200

    Added some vectorOperations.

--- 01-Vector/Vector.h
+++ 01-Vector/Vector.h
@@ -1,6 +1,57 @@
 #pragma once
-
+#include<cassert>
+template<typename T>
 class Vector {
-public :
+private:
+	int size;
+	T* arr = nullptr;
+public:
 	Vector(int size);
+	T get(const int index) const;
+	void set(const int index, const T number);
+	void print() const;
+	int find(T value);
 };
+
+template<typename T>
+inline Vector<T>::Vector(int size)
+{
+	assert(size > 0);
+	this->size = size;
+	arr = new T[size];
+}
+
+template<typename T>
+inline T Vector<T>::get(const int index) const
+{
+	assert(index >= 0 && index < size);
+	return arr[index];
+}
+
+template<typename T>
+inline void Vector<T>::set(const int index, const T number)
+{
+	assert(index >= 0 && index < size);
+	arr[index] = number;
+}
+
+template<typename T>
+inline void Vector<T>::print() const
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << ' ';
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+inline int Vector<T>::find(T value)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (value == arr[i])
+			return i;
+	}
+	return -1;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
-
+#include "01-Vector/Vector.h"
 using namespace std; 
 int main()
 {
-	cout << " Hello "; 
+	Vector<int> v1(3); 
+
+	v1.set(2, 5); 
+	v1.set(0, 1);
+	v1.set(1, 3); 
+
+	v1.print(); 
 }
