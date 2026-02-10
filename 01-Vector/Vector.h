0d40348987e5fe1f367f949a9e3cad4af35a8932
most3f3.1112006@gmail.com <most3f3.1112006@gmail.com>
2026-02-11 00:14:59 +0200

    Added RightRotation & LeftRotation & erase

--- 01-Vector/Vector.h
+++ 01-Vector/Vector.h
@@ -1,11 +1,13 @@
 #pragma once
 #include<cassert>
+#include<algorithm>
 template<typename T>
 class Vector {
 private:
 	int size{0}; // user size 
 	int capacity{0}; // actual size 
 	T* arr = nullptr;
+	void expand_capacity();
 public:
 	Vector(T size);
 	T at(const T index) const;
@@ -15,7 +17,11 @@ public:
 	void push_back(T number);
 	T front()const; 
 	T back() const;
-	void expand_capacity();
+	void insert(const size_t index, const T number);
+	void left_rotation(); 
+	void right_rotation(); 
+	T erase(int index) ; 
+	//int length();
 };
 
 template<typename T>
@@ -92,6 +98,73 @@ inline void Vector<T>::expand_capacity()
 	for (int i = 0; i < size; i++)
 		new_arr[i] = arr[i]; 
 	swap(arr, new_arr); // swap addresses not data 
-	delete[] arr;   
-	arr = new_arr;
+	delete[] new_arr;   
 }
+
+template<typename T>
+inline void Vector<T>::insert(const size_t index, const T number)
+{
+	assert(index >= 0 && index <= size);
+
+	if (size == capacity)
+		expand_capacity(); 
+	// Shifting to right untill index only
+
+	
+	for (int i = size - 1; i >= index; i--)
+		arr[i + 1] = arr[i]; // --> { 0 , 1 ,index, 2, 3} // index is empty pr carp Val
+	arr[index] = number; 
+	size++; 
+}
+
+template<typename T>
+inline void Vector<T>::left_rotation()
+{
+	T temp = this->front(); 
+	for (int i = 0; i < size - 1; i++)
+		arr[i] = arr[i + 1]; 
+	arr[size - 1] = temp; 
+}
+
+template<typename T>
+inline void Vector<T>::right_rotation()
+{
+	T temp = this->back(); 
+	for (int i = size - 2; i >= 0; i--)
+		arr[i + 1] = arr[i]; 
+	arr[0] = temp; 
+}
+
+template<typename T>
+inline T Vector<T>::erase(int index)
+{
+	assert(index >= 0 && index < size);
+	T* new_arr = new T[size]; 
+	T deletedNumber = 0; 
+	for (int i = 0; i < size; i++)
+	{
+		if (index == i)
+		{
+			new_arr[i] = arr[i + 1]; 
+			deletedNumber = arr[i]; 
+			i++; 
+		}
+		else
+		{
+			new_arr[i] = arr[i]; 
+		}
+	}
+	size--; 
+	swap(arr, new_arr); 
+	delete[] new_arr; 
+	return deletedNumber; 
+}
+
+
+
+//template<typename T>
+//inline int Vector<T>::length()
+//{
+//	return size; 
+//}
+
--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 using namespace std; 
 int main()
 {
-	int n = 3; 
+	int n = 5; 
 	Vector<int>v1(n); 
 	for (int i = 0; i < n; i++)
 	{
 		v1.set(i, i); 
 	}
-	v1.push_back(12.5);
-	
-	v1.print();
+	v1.erase(3); 
+	v1.print(); 
  }
