516ddb6c582bfbf45101da7ee73c6d96adbdf561
most3f3.1112006@gmail.com <most3f3.1112006@gmail.com>
2026-02-23 03:22:03 +0200

    Added MergeSort

--- Sorting Algorthims/Sorting.h
+++ Sorting Algorthims/Sorting.h
@@ -55,3 +55,56 @@ void BubbleSort(T* arr, int size)
 		}
 	}
 } 
+
+template<typename T>
+void merge(T* arr, size_t left, size_t middle ,size_t right)
+{
+	int size_arr1 = middle - left + 1; 
+	int size_arr2 = right - middle ; 
+
+	T* arr1 = new T[size_arr1]; 
+	T* arr2 = new T[size_arr2]; 
+	for (int i = 0; i < size_arr1; i++)
+		arr1[i] = arr[left+i]; 
+	for (int j = 0; j < size_arr2; j++)
+		arr2[j] = arr[middle+1+j];
+	size_t i, j;
+	i = j = 0;
+	size_t k = left; 
+	while (i < size_arr1 && j < size_arr2)
+	{
+		if (arr1[i] <= arr2[j])
+		{
+			arr[k] = arr1[i]; 
+			k++; i++;
+		}
+		else
+		{
+			arr[k] = arr2[j];
+			k++; j++;
+		}
+	}
+
+	while (i < size_arr1)
+	{
+		arr[k] = arr1[i]; 
+		k++; i++;
+	}
+	while (j < size_arr2)
+	{
+		arr[k] = arr2[j];
+		k++; j++;
+	}
+}
+
+template<typename T>
+void merge_sort(T* arr, size_t l, size_t r)
+{
+	if (l < r)
+	{
+		int mid = l + (r - l) / 2; 
+		merge_sort(arr, l, mid); 
+		merge_sort(arr,mid+1, r);
+		merge(arr, l, mid , r); 
+	}
+}
